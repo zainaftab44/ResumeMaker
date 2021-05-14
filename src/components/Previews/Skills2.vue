acf4537cf717d2e0cd10c3c8e1b992fca6715caf
Zain aftab <zain.aftab@z57.com>
2021-05-14 15:14:41 +0500

    ui fixes

--- src/components/Previews/Skills2.vue
+++ src/components/Previews/Skills2.vue
@@ -1,5 +1,7 @@
 <template>
     <div class="row">
+        <h4 style="margin-bottom:5px;padding-bottom:5px">SKILLS</h4>
+
         <span style="background-color:#bfbfbf; padding: 10px 15px;margin: 5px;border-radius: 5px; width:max-content" class="skills2" v-for="skname in skills2.name" :key="skname">{{ skname }}</span>
     </div>
 </template>
